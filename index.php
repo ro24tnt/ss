2d367c992fed8b378657ff2fe4694d7a9b94ea54
radu <radu@dotstudio.ro>
2018-02-26 16:07:00 +0200

    clean

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <div class="container np main-content">
-    <h1 class="vhidden">SapteSeri</h1>
+    <h1 class="vhidden" >SapteSeri</h1>
 
     <div class="widget widget-content more-on-site highlights">
         <?php echo Modules::run('template/places_list_block', $places_list); ?>
